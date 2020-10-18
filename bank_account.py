4c08513d89a3257d4b2681bed6467d41527ac3a2
Adam Ralph <adamralph@posteo.net>
2020-10-18 11:42:27 +1100

    Create classes objects and methods

--- bank_account.py
+++ bank_account.py
@@ -1,1 +1,32 @@
- 
+# Create a bank account program
+#
+#       That has two attributes:
+#
+#       - owner
+#       - balance
+#
+#       and two methods:
+#
+#       - deposit
+#       - withdraw
+#
+# As an added requitement, withdrawals may not exceed the available balance
+#
+# Instantiate your class, make several deposits and withdrawals, and test to make
+# sure the account can't be overdrawn.
+
+class Account():
+
+    def __init__(self,owner,balance):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self,amount):
+        self.ammount = amount
+        
+    def __str__(self,owner,balance):
+        print(f'Account owner: {self.owner}')
+        print(f'Account balance: {self.balance}')
+
+acct1 = Account('Adam',100)
+print(acct1)
