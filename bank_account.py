9d7bf0827ce28b07e7af56a98752936e1b6e9c81
Adam Ralph <adamralph@posteo.net>
2020-10-18 16:08:19 +1100

    Added opening dialog for user to est up a bank account

--- bank_account.py
+++ bank_account.py
@@ -12,19 +12,30 @@
 #
 # As an added requitement, withdrawals may not exceed the available balance
 #
-# Instantiate your class, make several deposits and withdrawals, and test to make
-# sure the account can't be overdrawn.
+# Instantiate your class, make several deposits and withdrawals
+# and test to make sure the account can't be overdrawn.
 
 class Account():
 
-    def __init__(self,owner,balance):
+    def __init__(self, owner, balance):
         self.owner = owner
         self.balance = balance
 
-    def deposit(self,amount):
-        self.ammount = amount
+    def deposit(self, amount):
+        self.amount = amount
 
     def __str__(self):
         return f'Account owner: {self.owner}\nAccount balance: {self.balance}'
-acct1 = Account('Adam',100)
+
+
+
+print('Hello and welcome to the bank account')
+client = input('Please enter the account holder: ')
+print(f'Account holder: {client}')
+opening_balance = input('Please enter the opening balance: ')
+acct1 = Account(client, opening_balance) 
+
 print(acct1)
+
+# Account(client)
+
