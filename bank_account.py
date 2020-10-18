abdd01c66ad1d06054872b65d5e2f43fcc0fd9c4
Adam Ralph <adamralph@posteo.net>
2020-10-18 18:26:24 +1100

    Fixed various formatting issues

--- bank_account.py
+++ bank_account.py
@@ -16,6 +16,15 @@
 # and test to make sure the account can't be overdrawn.
 
 class Account():
+    '''
+    Creates a bank account with an account balance.
+    The owner and opening balance are specified by the
+    user.
+    Amounts can be withdrawn or deposited, but withdrawals
+    can not exceed available balance.
+    Assumes owner - a string
+    and balance, a float.
+    '''
 
     def __init__(self, owner, balance):
         self.owner = owner
@@ -28,14 +37,12 @@ class Account():
         return f'Account owner: {self.owner}\nAccount balance: {self.balance}'
 
 
-
 print('Hello and welcome to the bank account')
+
+
 client = input('Please enter the account holder: ')
 print(f'Account holder: {client}')
 opening_balance = input('Please enter the opening balance: ')
-acct1 = Account(client, opening_balance) 
+acct1 = Account(client, opening_balance)
 
 print(acct1)
-
-# Account(client)
-
